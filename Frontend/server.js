5f8ec385d8f98720a6edac1e2f0e378e160a8975
Suttipot Prathoumthong - 6609650699 <144650601+SuttipotPrathoumthong@users.noreply.github.com>
2024-11-11 20:57:04 +0700

    Update capital sensitive server.js

--- Frontend/server.js
+++ Frontend/server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'login.html'));
+    res.sendFile(path.join(__dirname, 'public', 'Login.html'));
 });
 
 const PORT = process.env.PORT || 5501;
